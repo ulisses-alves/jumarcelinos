83613fbb3dcba3ac70066cfd61ccdb12c01de549
Ulisses Bini de Paiva Alves <ulissesbpalves@gmail.com>
2017-05-29 19:11:40 +0200

    Update topic texts and color.

--- src/elm/Pages/Home.elm
+++ src/elm/Pages/Home.elm
@@ -21,20 +21,23 @@ contentView =
     [ articleView
         "images/topic_1.png"
         "\"Create engaging content and tell your unic story.\""
+        "Prove business results and unlock the value of your content. I can help you with beautiful custom content, that will build lasting and meaningful relationships with customers."
         False
     , articleView
         "images/topic_2.png"
         "\"Best articulate your brand with the right strategy.\""
+        "Best articulate who you are, what you do and most importantly why you do it. With the right strategy you can express your brand genuine personality."
         True
     , articleView
         "images/topic_3.png"
         "\"Bring brand values to your intranet.\""
+        "You need an internal communication that engages employees and changes behavior. Help your employees live the brand."
         False
     ]
 
 
-articleView : String -> String -> Bool -> Html msg
-articleView imgUrl txt reversed =
+articleView : String -> String -> String -> Bool -> Html msg
+articleView imgUrl heading content reversed =
   let
     image =
       div
@@ -50,11 +53,11 @@ articleView imgUrl txt reversed =
       div
         [ class "sm-col sm-col-6 px2 sm-px4" ]
         [ h2
-            [ class "h1 m0" ]
-            [ text txt
+            [ class "h1 m0 red" ]
+            [ text heading
             ]
         , p []
-            [ text "Vivamus sed leo et lorem dapibus finibus sit amet a odio. Etiam ac molestie magna."
+            [ text content
             ]
         ]
   in
